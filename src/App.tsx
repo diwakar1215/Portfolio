5491e136bd5f708244d408c8684b22bb498d3b7d
diwakar1215 <diwakar9345@gmail.com>
2026-08-16 19:24:59 +0530

    fix: remove unused Router import causing ts build failure

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { lazy, Suspense } from "react";
-import { HashRouter as Router, Routes, Route, BrowserRouter } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.css";
 
 const CharacterModel = lazy(() => import("./components/Character"));
